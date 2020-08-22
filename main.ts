3df1413351d360069ed5e6a20438858973b998da
Rogier van den Berg <rogiervandenberg@gmail.com>
2020-08-22 17:18:20 +0200

    Fix 'URL' is not assignable to type 'string' error

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 import "https://deno.land/x/dotenv/mod.ts";
 
-const PORT = Deno.env.get('PORT') || 8080;
+const PORT = Deno.env.get("PORT") || 8080;
 const s = serve(`0.0.0.0:${PORT}`);
 const body = new TextEncoder().encode("Hello World, this is Deno\n");
 
